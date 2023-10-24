59f78ebc62c39b9f3e0aaf81a4de97f9694c08fd
rikkunfops <100575126@unimail.derby.ac.uk>
2023-10-24 10:42:00 +0100

    begin building interface

---
+++ WindowsApp.py
@@ -0,0 +1,26 @@
+from tkinter import *
+import ctypes
+
+#Get screen size
+user32 = ctypes.windll.user32
+screenwidth = user32.GetSystemMetrics(0)
+screenheight = user32.GetSystemMetrics(1)
+print(screenheight, screenwidth)
+#initialize window
+root = Tk()
+root.title("DistributedSystem Program")
+root.configure(background="cyan")
+root.minsize(200,200)
+root.geometry("300x300+50+50")
+
+
+#Create label
+text = Label(root, text="lol", background="green")
+text.pack()
+text2 = Label(root, text="lmao, even")
+text2.pack()
+image=PhotoImage(file="graphical assets/mrmoulder.gif")
+img = Label(root, image=image)
+img.pack()
+
+root.mainloop()
